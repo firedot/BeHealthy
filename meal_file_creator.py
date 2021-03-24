682db021db8c8cef64738fde95ea58698411dfa0
Aleksandar Todoro <linked2u@gmail.com>
2021-03-24 14:01:13 +0200

    renamed file

---
+++ meal_file_creator.py
@@ -0,0 +1,12 @@
+import json
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--manual', '-m',
+                    metavar = 'Choose manual mode of operation'
+                    )
+
+
