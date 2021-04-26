3343478e8005c58d0e821f38a8bbc58eb2abcdad
Aleksandar Todoro <linked2u@gmail.com>
2021-04-26 12:03:06 +0300

    added function that creates meal; added object that handles meal files

--- meal_file_creator.py
+++ meal_file_creator.py
@@ -14,25 +14,47 @@ mealfiles = ['breakfast.json', 'lunch.json', 'dinner.json', 'snack.json']
 
 class mealFile:
 
-    data = {}
+    def _readFile(file):
+        with open(file, 'r') as input_file:
+            return json.loads(input_file.read())
 
-    def _readFile(filename):
-        # Open previously created file
-        with open(filename, 'r') as input_file:
-            self.data = json.loads(input_file.read())
+    def __init__(self, file_name):
+        self.file_name = file_name
+        if os.path.exists(file_name):
+            self.body = _readFile(self.file_name)
+        else:
+            self.body = dict(title='mealfile', version=1, day_meals=[])
 
-    def __init__(self, name):
-        self.name = name
-        if os.path.exists(self.name):
-            _readFile(self.name)
-
-    def addMeal(self.data):
+    def addMeal(self, meal: dict):
+        """
+        adds a dictionary containing a meal
+        to the meal file.
+        The meal is structured like follows:
+            {meal_type: brekfast/snack/lunch/dinner,
+             ingredients: {ingredient_1: qty, ingredient_2: qty},
+             nutrition_day: 1-30,
+             meal_variant: 1-10
+             }
+        """
         pass
 
     def writeFile():
         with open(self.name, 'w') as output_file:
-            output_file.write(json.dumps(self.data))
+            if not os.path.exists(self.name):
 
+# Define functions
+
+def CreateMeal(meal_type: str, ingredients: dict,
+               nutrition_day: int,
+               meal_variant: int):
+
+    meal = dict()
+    meal['meal_type'] = meal_type
+    meal['ingredients'] = ingredients
+    meal['nutrition_day'] = nutrition_day
+    meal['meal_variant'] = meal_variant
+
+    return meal
 
 # Check if meal files exist
 
