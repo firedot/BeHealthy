a4b5012b7c224918bb46772df9107c89a1d00ae5
Aleksandar Todoro <linked2u@gmail.com>
2021-04-26 17:55:52 +0300

    fixing errors

--- meal_file_creator.py
+++ meal_file_creator.py
@@ -6,11 +6,12 @@ from random import randint
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--manual', '-m',
-                    metavar = 'Choose manual mode of operation'
+                    metavar='Choose manual mode of operation',
+                    action='store_true'
                     )
 
 parser.add_argument('--filename', '-f',
-                    metavar = 'Name of the meal file'
+                    metavar='Name of the meal file'
                     )
 
 args = parser.parse_args()
@@ -41,11 +42,12 @@ class mealFile:
              meal_variant: 1-10
              }
         """
-       self.body['day_meals'].append(meal)
+        self.body['day_meals'].append(meal)
 
     def writeFile():
         with open(self.name, 'w') as output_file:
             if not os.path.exists(self.name):
+                pass
 
 # Define functions
 
@@ -53,20 +55,20 @@ def CreateMeal(meal_type: str, ingredients: dict,
                nutrition_day: int,
                meal_variant: int):
 
-    meal = dict()
-    meal['meal_type'] = meal_type
-    meal['ingredients'] = ingredients
-    meal['nutrition_day'] = nutrition_day
-    meal['meal_variant'] = meal_variant
+     meal = dict()
+     meal['meal_type'] = meal_type
+     meal['ingredients'] = ingredients
+     meal['nutrition_day'] = nutrition_day
+     meal['meal_variant'] = meal_variant
 
-    return meal
+     return meal
 
 
 if __name__ == "__main__":
     done = False
     meal_1 = mealFile(args.filename)
 
-    if args.m == 'm':
+    if args.m:
         while not done:
             m_type = input('Please enter the meal type: ')
             ingredients = input('Please add ingredients. I.e. food:qty: ')
@@ -74,7 +76,7 @@ if __name__ == "__main__":
             m_variant = int(input('Please add meal variant number: '))
             meal = CreateMeal(m_type, ingredients, n_day, m_variant)
             meal_1.addMeal(meal)
-            end = input('Do you want to continue (Y\N): ')
+            end = input('Do you want to continue (Y\\N): ')
             if end == 'N':
                 print('Finished adding meals.')
                 done = True
