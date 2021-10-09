c4d87c77c42fc2fbafb4ab21cf073411fee84be7
Aleksandar Todorov <linked2u@gmail.com>
2021-10-09 17:17:00 +0300

    Completed Meal class with comments

--- packages/meal/meal.py
+++ packages/meal/meal.py
@@ -1,18 +1,46 @@
-'''
-DOD: Class which will hold 
-     - Ingredients
-     - quantity
-     - meal type
-     - meal varaint no.
-     - may be prep notes
-     Methods:
-        add ingredinet: dict(name, qty)
-        return ingredients: list(dict(name:qty))
-'''
-
 class Meal:
+   '''
+   A class used to represent a singe meal
 
-   def __init__(self, m_type, m_variant):
+   Attributes
+   ----------
+
+   meal_types: str
+      string of acceptable m_type values
+
+   m_type: str
+      type of the meal
+
+   m_variant: int
+      variant of the meal
+
+   dop: int
+       For which day of the nutrition plan the meal
+       is planned
+
+   Methods
+   -------
+
+   addIngredient(ingredient, qty)
+      Adds an ingredient to the meal's ingredients list
+
+   addNotes(notes)
+      Adds a note for the meal
+
+   getMealIngredients()
+      Returns a list of dictionaries
+      containing all ingredients and quantities
+
+   '''
+
+   meal_types = 'breakfast / lunch / snack / dinner'
+
+   def __init__(self, m_type: str, m_variant: int, dop: int):
+      if not m_type.lower() in self.meal_types:
+         raise ValueError(f'Meal type must be one of: {self.meal_types}')
+      elif not type(m_variant) is int or not type(dop) is int:
+         raise TypeError('m_variant and dop must be an integers')
+      else:
        self.ingredients = list()
        self.m_type = m_type
        self.m_variant = m_variant
@@ -20,8 +48,11 @@ class Meal:
 
    def addIngredient(self, ingredient: str, qty: int):
       '''Adds ingredinet entry to ingredients'''
-      entry = {ingredient:qty}
-      self.ingredients.append(entry)
+      if not type(qty) is int:
+         raise TypeError('Ingredient quantity must be an integer')
+      else:
+          entry = {ingredient:qty}
+          self.ingredients.append(entry)
 
    def addNotes(self, notes: str):
       '''Adds meal prep notes if any'''
