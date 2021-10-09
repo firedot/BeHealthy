2eb070630e61e704edd053f5d20b22d94fae0118
Aleksandar Todorov <linked2u@gmail.com>
2021-10-09 16:32:10 +0300

    Added class Meal

--- packages/meal/meal.py
+++ packages/meal/meal.py
@@ -18,9 +18,17 @@ class Meal:
        self.m_variant = m_variant
        self.notes = 'Nothing to see here! Move along...'
 
-   def addIngredient(ingredient, qty):
+   def addIngredient(self, ingredient: str, qty: int):
+      '''Adds ingredinet entry to ingredients'''
       entry = {ingredient:qty}
       self.ingredients.append(entry)
 
+   def addNotes(self, notes: str):
+      '''Adds meal prep notes if any'''
+      self.notes = notes
 
 
+   def getMealIngredients(self):
+      '''Returns list containing
+         dict entries for each ingredinet'''
+      return self.ingredients
