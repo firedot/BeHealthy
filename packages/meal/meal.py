eff9f7e70bbfa77486e7e2a8318eb9cc09b08bae
Aleksandar Todorov <linked2u@gmail.com>
2021-10-09 22:16:41 +0300

    refactoring of class Meal()

--- packages/meal/meal.py
+++ packages/meal/meal.py
@@ -1,39 +1,5 @@
 class Meal:
-   '''
-   A class used to represent a singe meal
-
-   Attributes
-   ----------
-
-   meal_types: str
-      string of acceptable m_type values
-
-   m_type: str
-      type of the meal
-
-   m_variant: int
-      variant of the meal
-
-   dop: int
-       For which day of the nutrition plan the meal
-       is planned
-
-   Methods
-   -------
-
-   addIngredient(ingredient, qty)
-      Adds an ingredient to the meal's ingredients list
-
-   addNotes(notes)
-      Adds a note for the meal
-
-   getMealIngredients()
-      Returns a list of dictionaries
-      containing all ingredients and quantities
-
-   '''
-
-   meal_types = ['breakfast','lunch','snack','dinner']
+   __meal_types = ['breakfast','lunch','snack','dinner']
 
    def __init__(self, m_type: str, m_variant: int, dop: int):
        self.ingredients = list()
@@ -43,11 +9,13 @@ class Meal:
        self.notes = 'Nothing to see here! Move along...'
 
    def __set_MealType(self, menu_type):
-      if not menu_type.lower() in self.meal_types:
-         raise ValueError(f'Meal type must be one of: {self.meal_types}')
+      '''Set the m_variant attribute'''
+      if not menu_type.lower() in self.__meal_types:
+         raise ValueError(f'Meal type must be one of: {self.__meal_types}')
       self.m_type = menu_type
 
    def __set_MealVariant(self, variant):
+      '''Set the m_variant attribute'''
       if not type(variant) is int:
          raise TypeError('m_variant must be an integer')
       self.m_variant = variant
@@ -72,5 +40,5 @@ class Meal:
 
    def getMealIngredients(self):
       '''Returns list containing
-         dict entries for each ingredinet'''
+         dict entries for each ingredient'''
       return self.ingredients
